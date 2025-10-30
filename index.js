ec5e1b5813bdf8e0b6e721aa1a4d35eddb4df49c
Sergey <hajoff@gmail.com>
2025-10-30 14:08:07 +0200

    Add gitignore and update rules

--- index.js
+++ index.js
@@ -38,7 +38,15 @@ const nomeusRules = {
 	}],
 	'vue/block-order': ['error', {
 		'order': ['script', 'template', 'style']
-	}]
+	}],
+	'padding-line-between-statements': [
+		'error', { 'blankLine': 'always', 'prev': '*', 'next': 'return' },
+		{
+			'blankLine': 'always',
+			'prev': '*',
+			'next': ['if', 'while', 'for', 'switch', 'try']
+		}
+	]
 };
 
 export default [
